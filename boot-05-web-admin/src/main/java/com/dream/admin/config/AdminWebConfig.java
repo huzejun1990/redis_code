73ad23db1372b5e14a87da8870200c44ec0ba1c9
huzejun <961639130@qq.com>
2021-04-27 00:41:51 +0800

    springboot2.4.x curd 初步完成 2021-4-27 0:41:18

--- boot-05-web-admin/src/main/java/com/dream/admin/config/AdminWebConfig.java
+++ boot-05-web-admin/src/main/java/com/dream/admin/config/AdminWebConfig.java
@@ -18,7 +18,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * 3、指定拦截规则【如果是拦截所有，静态资源也会被拦截】
  *
  * @EnableWebMvc:全面接管
- *        1、静态资源？
+ *        1、静态资源？视图解析器？欢迎页面。。。。全部失效
  *
  */
 //@EnableWebMvc
@@ -29,22 +29,22 @@ public class AdminWebConfig implements WebMvcConfigurer {
      * 定义静态资源行为
      * @param registry
      */
-//    @Override
-//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        /**
-//         * 访问 /aa/** 所有请求都去 classpath:/static/ 下面进行匹配
-//         */
-//        registry.addResourceHandler("/aa/**")
-//                .addResourceLocations("classpath:/static/**");
-//
-//        WebMvcConfigurer.super.addResourceHandlers(registry);
-//    }
+/*    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        *//**
+         * 访问 /aa/** 所有请求都去 classpath:/static/ 下面进行匹配
+         *//*
+        registry.addResourceHandler("/aa/**")
+                .addResourceLocations("classpath:/static/**");
+
+        WebMvcConfigurer.super.addResourceHandlers(registry);
+    }*/
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoginInterceptor())
                 .addPathPatterns("/**") //所有请求都被拦截包括所有请求
                 .excludePathPatterns("/","/login","/css/**","/fonts/**","/images/**",
-                        "/js/**","/aa/**","/sql","/city"); ///login    //放行的请求
+                        "/js/**","/aa/**","/sql","/city","/my"); ///login    //放行的请求
     }
 }
--- boot-05-web-admin/src/main/java/com/dream/admin/controller/TableController.java
+++ boot-05-web-admin/src/main/java/com/dream/admin/controller/TableController.java
@@ -1,13 +1,16 @@
 package com.dream.admin.controller;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.dream.admin.bean.User;
-import com.dream.admin.exception.UserTooManyException;
+import com.dream.admin.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -16,6 +19,9 @@ import java.util.List;
  */
 @Controller
 public class TableController {
+
+    @Autowired
+    UserService userService;
 
     /**
      *
@@ -29,10 +35,23 @@ public class TableController {
         return "table/basic_table";
     }
 
+    @GetMapping("/user/delete/{id}")
+    public String deleteUser(@PathVariable("id") Long id,
+                             @RequestParam(value = "pn",defaultValue = "1") Integer pn,
+                             RedirectAttributes redirectAttributes){
+
+        userService.removeById(id);
+
+        redirectAttributes.addAttribute("pn", pn);
+
+        return "redirect:/dynamic_table";
+
+    }
+
     @GetMapping("/dynamic_table")
-    public String dynamic_table(Model model){
+    public String dynamic_table(@RequestParam(value = "pn",defaultValue = "1")Integer pn, Model model){
         //表格内容的遍历
-        List<User> users = Arrays.asList(new User("zhangsan", "123456"),
+/*        List<User> users = Arrays.asList(new User("zhangsan", "123456"),
                 new User("lisi", "123444"),
                 new User("haha", "aaa"),
                 new User("hehe", "aaadddd"));
@@ -41,7 +60,24 @@ public class TableController {
             throw new UserTooManyException();
         }
 
-        model.addAttribute("users",users);
+        model.addAttribute("users",users);*/
+        //从数据库中查出user表中的用户进行展示
+
+        List<User> list = userService.list();
+
+//        model.addAttribute("users",list);
+
+        //分页查询出的数据
+        Page<User> userPage = new Page<>(pn, 2);
+
+        //分页查询的结果
+        Page<User> page = userService.page(userPage, null);
+        long current = page.getCurrent();
+        long pages = page.getPages();
+        long total = page.getTotal();
+        List<User> records = page.getRecords();
+
+        model.addAttribute("page",page);
 
         return "table/dynamic_table";
     }
