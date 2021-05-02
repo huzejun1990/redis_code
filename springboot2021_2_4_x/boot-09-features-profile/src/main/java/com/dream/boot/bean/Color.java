6db5c37ac91ab61355807183b3fd45e458ae3d76
huzejun <961639130@qq.com>
2021-05-02 18:04:17 +0800

    springboot 配置加载优先级（主要是application和@Value注解）

---
+++ springboot2021_2_4_x/boot-09-features-profile/src/main/java/com/dream/boot/bean/Color.java
@@ -0,0 +1,10 @@
+package com.dream.boot.bean;
+
+/**
+ * @ClassName : Color
+ * @Author : huzejun
+ * @Date: 2021/4/30-23:06
+ * @Description :
+ */
+public class Color {
+}
---
+++ springboot2021_2_4_x/boot-09-features-profile/src/main/java/com/dream/boot/bean/Person.java
@@ -0,0 +1,16 @@
+package com.dream.boot.bean;
+
+/**
+ * @ClassName : Person
+ * @Author : huzejun
+ * @Date: 2021/4/30-22:55
+ * @Description :
+ */
+
+public interface Person {
+
+    String getName();
+    Integer getAge();
+
+
+}
